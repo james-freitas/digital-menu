e173dba275d7e89be36d044272ebe58287547024
James Freitas <jamesfrj@yahoo.com.br>
2024-01-27 12:29:24 -0300

    Add Home page component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Saudacao from './components/saudacao/saudacao';
 import "./style/global.css";
+import Home from './pages/home/home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Saudacao nome="Jason"/>
+    <Home/>
 );
 
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return <> 
+    Navbar...<br/>
+    Nosso Cardápio<br/>
+    Clique em adicionar para colocar os produtos na sacola de compras. 
+    Se preferir, você pode pedir pelo WhatsApp: (11) 94785-2140<br/>
+  </>
+}
+
+export default Home;
