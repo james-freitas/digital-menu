0fa5c930b4a49d81b6b11f2a377c501e0c76c2a6
James Freitas <jamesfrj@yahoo.com.br>
2024-01-27 15:57:43 -0300

    Add home titles and media-queries formatting

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,9 +3,13 @@ import Navbar from "../../components/navbar/navbar";
 function Home() {
   return <> 
     <Navbar />
-    Nosso Cardápio<br/>
-    Clique em adicionar para colocar os produtos na sacola de compras. 
-    Se preferir, você pode pedir pelo WhatsApp: (11) 94785-2140<br/>
+
+    <div className="container">
+      <div className="titulo text-center">
+        <h1>Nosso Cardápio</h1>
+        <p className="subtitulo">Clique em adicionar para colocar os produtos na sacola de compras. Se preferir, você pode pedir pelo WhatsApp: (11) 94785-2140</p>
+      </div>
+    </div>
   </>
 }
 
