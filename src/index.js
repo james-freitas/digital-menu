dbc36feb4b9e8d0f98562532e2ed001663b5f9a0
James Freitas <jamesfrj@yahoo.com.br>
2024-01-27 17:12:29 -0300

    Install route lib and configure routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./style/global.css";
-import Home from './pages/home/home';
+import Rotas from './rotas';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Home/>
+    <Rotas />
 );
 
