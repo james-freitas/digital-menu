b3c80e558089f7a3840063db4e08c3ec215298f0
James Freitas <jamesfrj@yahoo.com.br>
2024-01-28 07:07:25 -0300

    Map data from json to a table and format BR currency

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -7,7 +7,7 @@ function Home() {
 
     <div className="container">
       <div className="titulo text-center">
-        <h1>Nosso Cardápio</h1>
+        <h1>Nosso Cardápio!</h1>
         <p className="subtitulo">Clique em adicionar para colocar os produtos na sacola de compras. Se preferir, você pode pedir pelo WhatsApp: (11) 94785-2140</p>
       </div>
     </div>
